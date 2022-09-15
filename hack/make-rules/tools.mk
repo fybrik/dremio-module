722d1c02563e63b6f5ff68d39b759fca0df3a395
mohammad-nassar10 <mohammad.nassar@ibm.com>
2022-09-15 12:38:17 +0300

    Undo removing yq.
    Signed-off-by: mohammad-nassar10 <mohammad.nassar@ibm.com>

--- hack/make-rules/tools.mk
+++ hack/make-rules/tools.mk
@@ -1,7 +1,14 @@
+INSTALL_TOOLS += $(TOOLBIN)/yq
+$(TOOLBIN)/yq:
+	cd $(TOOLS_DIR); ./install_yq.sh
+	$(call post-install-check)
+
 INSTALL_TOOLS += $(TOOLBIN)/helm
 $(TOOLBIN)/helm:
 	cd $(TOOLS_DIR); ./install_helm.sh
 	$(call post-install-check)
+
+
 
 .PHONY: install-tools
 install-tools: $(INSTALL_TOOLS)
